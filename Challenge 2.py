ecdc4b6851974467779fd5249c433a26b162ddb1
alvaropande <alvaro.pande@gmail.com>
2026-01-14 22:02:04 +0800

    Add files via upload

---
+++ Challenge 2.py
@@ -0,0 +1,7 @@
+player_health = 100
+health_potion = 25
+monster_damage = -40
+player_health = player_health + 25
+print(player_health)
+player_health = player_health - 40
+print(player_health)
---
+++ Project_bot.py
@@ -0,0 +1,7 @@
+print("Hello! Im a simple chatbot. Let's get to know each other.")
+Name = input("What's your name?:")
+Age = input("How old are you?:")
+Food = input("What is your favorite food?:")
+Game = input("What is your favorite game?:")
+Answer = f"So your name is {Name}, you are {Age} years old, loves eating {Food} and playing {Game}, nice to meet you!."
+print(Answer)
